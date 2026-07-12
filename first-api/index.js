c5324c82d26528d9862c6bd26704955c5a9ff3b5
azeem40001-max <azeem40001@gmail.com>
2026-07-12 03:40:42 +0300

    Initial commit

---
+++ first-api/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Hello, World"
+    });
+});
+
+app.get("/login", (req, res) => {
+    res.json({
+        status: "ok"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
